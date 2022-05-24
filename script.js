3176377cfc5e9cb68bbd012ba1280ad079524448
Vinicius Benso <vinibenso@gmail.com>
2022-05-24 16:51:17 -0300

    alteracao teste

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ const container = document.querySelector('.container')
 open.addEventListener('click',() => container.classList.add ('show-nav'))
 
 close.addEventListener('click',() => container.classList.remove ('show-nav'))
+
+//teste commit
+console.log('Oi')
