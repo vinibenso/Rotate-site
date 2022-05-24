e486eab2519e100eddad5a7550f185a1b07147b1
Vinicius Benso <104365736+vinibenso@users.noreply.github.com>
2022-05-24 16:52:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,3 @@ open.addEventListener('click',() => container.classList.add ('show-nav'))
 
 close.addEventListener('click',() => container.classList.remove ('show-nav'))
 
-//teste commit
-console.log('Oi')
